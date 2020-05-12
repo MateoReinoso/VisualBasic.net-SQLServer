d58f9061c6afe2e0a5eed61937379460e7e56980
MateoReinoso <dmreinoso2@espe.edu.ec>
2020-05-11 23:21:09 -0500

    Calculadora en VB.net

--- Aplicacion/PrimerProyecto/PrimerProyecto/Ejercicio.Designer.vb
+++ Aplicacion/PrimerProyecto/PrimerProyecto/Ejercicio.Designer.vb
@@ -22,9 +22,148 @@ Partial Class Ejercicio
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.lblN1 = New System.Windows.Forms.Label()
+        Me.lblN2 = New System.Windows.Forms.Label()
+        Me.txtNum1 = New System.Windows.Forms.TextBox()
+        Me.txtNum2 = New System.Windows.Forms.TextBox()
+        Me.btnSuma = New System.Windows.Forms.Button()
+        Me.lblResultado = New System.Windows.Forms.Label()
+        Me.txtResultado = New System.Windows.Forms.TextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.btnResta = New System.Windows.Forms.Button()
+        Me.btnMultiplicacion = New System.Windows.Forms.Button()
+        Me.btnDivision = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lblN1
+        '
+        Me.lblN1.AutoSize = True
+        Me.lblN1.Location = New System.Drawing.Point(141, 88)
+        Me.lblN1.Name = "lblN1"
+        Me.lblN1.Size = New System.Drawing.Size(56, 13)
+        Me.lblN1.TabIndex = 0
+        Me.lblN1.Text = "Numero 1:"
+        '
+        'lblN2
+        '
+        Me.lblN2.AutoSize = True
+        Me.lblN2.Location = New System.Drawing.Point(141, 148)
+        Me.lblN2.Name = "lblN2"
+        Me.lblN2.Size = New System.Drawing.Size(56, 13)
+        Me.lblN2.TabIndex = 1
+        Me.lblN2.Text = "Numero 2:"
+        '
+        'txtNum1
+        '
+        Me.txtNum1.Location = New System.Drawing.Point(218, 81)
+        Me.txtNum1.Name = "txtNum1"
+        Me.txtNum1.Size = New System.Drawing.Size(100, 20)
+        Me.txtNum1.TabIndex = 2
+        '
+        'txtNum2
+        '
+        Me.txtNum2.Location = New System.Drawing.Point(218, 141)
+        Me.txtNum2.Name = "txtNum2"
+        Me.txtNum2.Size = New System.Drawing.Size(100, 20)
+        Me.txtNum2.TabIndex = 3
+        '
+        'btnSuma
+        '
+        Me.btnSuma.Font = New System.Drawing.Font("Cambria", 17.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSuma.Location = New System.Drawing.Point(407, 59)
+        Me.btnSuma.Name = "btnSuma"
+        Me.btnSuma.Size = New System.Drawing.Size(61, 60)
+        Me.btnSuma.TabIndex = 4
+        Me.btnSuma.Text = "+"
+        Me.btnSuma.UseVisualStyleBackColor = True
+        '
+        'lblResultado
+        '
+        Me.lblResultado.AutoSize = True
+        Me.lblResultado.Location = New System.Drawing.Point(141, 215)
+        Me.lblResultado.Name = "lblResultado"
+        Me.lblResultado.Size = New System.Drawing.Size(55, 13)
+        Me.lblResultado.TabIndex = 5
+        Me.lblResultado.Text = "Resultado"
+        '
+        'txtResultado
+        '
+        Me.txtResultado.Location = New System.Drawing.Point(218, 208)
+        Me.txtResultado.Name = "txtResultado"
+        Me.txtResultado.Size = New System.Drawing.Size(100, 20)
+        Me.txtResultado.TabIndex = 6
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.btnDivision)
+        Me.Panel1.Controls.Add(Me.btnMultiplicacion)
+        Me.Panel1.Controls.Add(Me.btnResta)
+        Me.Panel1.Controls.Add(Me.txtResultado)
+        Me.Panel1.Controls.Add(Me.lblResultado)
+        Me.Panel1.Controls.Add(Me.btnSuma)
+        Me.Panel1.Controls.Add(Me.txtNum2)
+        Me.Panel1.Controls.Add(Me.txtNum1)
+        Me.Panel1.Controls.Add(Me.lblN2)
+        Me.Panel1.Controls.Add(Me.lblN1)
+        Me.Panel1.Location = New System.Drawing.Point(118, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(552, 344)
+        Me.Panel1.TabIndex = 0
+        '
+        'btnResta
+        '
+        Me.btnResta.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnResta.Location = New System.Drawing.Point(407, 125)
+        Me.btnResta.Name = "btnResta"
+        Me.btnResta.Size = New System.Drawing.Size(61, 50)
+        Me.btnResta.TabIndex = 7
+        Me.btnResta.Text = "-"
+        Me.btnResta.UseVisualStyleBackColor = True
+        '
+        'btnMultiplicacion
+        '
+        Me.btnMultiplicacion.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMultiplicacion.Location = New System.Drawing.Point(407, 181)
+        Me.btnMultiplicacion.Name = "btnMultiplicacion"
+        Me.btnMultiplicacion.Size = New System.Drawing.Size(61, 47)
+        Me.btnMultiplicacion.TabIndex = 8
+        Me.btnMultiplicacion.Text = "x"
+        Me.btnMultiplicacion.UseVisualStyleBackColor = True
+        '
+        'btnDivision
+        '
+        Me.btnDivision.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDivision.Location = New System.Drawing.Point(407, 246)
+        Me.btnDivision.Name = "btnDivision"
+        Me.btnDivision.Size = New System.Drawing.Size(61, 51)
+        Me.btnDivision.TabIndex = 9
+        Me.btnDivision.Text = "%"
+        Me.btnDivision.UseVisualStyleBackColor = True
+        '
+        'Ejercicio
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Ejercicio"
         Me.Text = "Ejercicio"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
     End Sub
+
+    Friend WithEvents lblN1 As Label
+    Friend WithEvents lblN2 As Label
+    Friend WithEvents txtNum1 As TextBox
+    Friend WithEvents txtNum2 As TextBox
+    Friend WithEvents btnSuma As Button
+    Friend WithEvents lblResultado As Label
+    Friend WithEvents txtResultado As TextBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents btnResta As Button
+    Friend WithEvents btnDivision As Button
+    Friend WithEvents btnMultiplicacion As Button
 End Class
--- Aplicacion/PrimerProyecto/PrimerProyecto/Ejercicio.vb
+++ Aplicacion/PrimerProyecto/PrimerProyecto/Ejercicio.vb
@@ -1,3 +1,58 @@
 ﻿Public Class Ejercicio
 
+    Private Sub Ejercicio_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnSuma_Click(sender As Object, e As EventArgs) Handles btnSuma.Click
+        Dim numero1 As Decimal
+        Dim numero2 As Integer
+        Dim resultado As Integer
+
+        numero1 = txtNum1.Text
+        numero2 = txtNum2.Text
+
+        resultado = numero1 + numero2
+
+        txtResultado.Text = resultado
+    End Sub
+
+    Private Sub btnResta_Click(sender As Object, e As EventArgs) Handles btnResta.Click
+        Dim numero1 As Integer
+        Dim numero2 As Integer
+        Dim resultado As Integer
+
+        numero1 = txtNum1.Text
+        numero2 = txtNum2.Text
+
+        resultado = numero1 - numero2
+
+        txtResultado.Text = resultado
+    End Sub
+
+    Private Sub btnMultiplicacion_Click(sender As Object, e As EventArgs) Handles btnMultiplicacion.Click
+        Dim numero1 As Integer
+        Dim numero2 As Integer
+        Dim resultado As Integer
+
+        numero1 = txtNum1.Text
+        numero2 = txtNum2.Text
+
+        resultado = numero1 * numero2
+
+        txtResultado.Text = resultado
+    End Sub
+
+    Private Sub btnDivision_Click(sender As Object, e As EventArgs) Handles btnDivision.Click
+        Dim numero1 As Integer
+        Dim numero2 As Integer
+        Dim resultado As Integer
+
+        numero1 = txtNum1.Text
+        numero2 = txtNum2.Text
+
+        resultado = numero1 / numero2
+
+        txtResultado.Text = resultado
+    End Sub
 End Class
