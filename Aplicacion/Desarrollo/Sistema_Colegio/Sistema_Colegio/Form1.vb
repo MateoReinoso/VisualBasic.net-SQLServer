23d910a94125f0f983926f33c673877a8baf2fc2
MateoReinoso <dmreinoso2@espe.edu.ec>
2020-05-12 17:25:06 -0500

    Usuario parte 1

--- Aplicacion/Desarrollo/Sistema_Colegio/Sistema_Colegio/Form1.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class frmPrincipal
-    Private Sub btnCerrar_Click(sender As Object, e As EventArgs) Handles btnCerrar.Click
-        Close()
-    End Sub
-
-End Class
---
+++ Aplicacion/Desarrollo/Sistema_Colegio/Sistema_Colegio/Login.vb
@@ -0,0 +1,24 @@
+﻿Public Class frmPrincipal
+    Private Sub btnCerrar_Click(sender As Object, e As EventArgs) Handles btnCerrar.Click
+        Close()
+    End Sub
+
+    Private Sub txtUsuario_TextChanged(sender As Object, e As EventArgs) Handles txtUsuario.TextChanged
+
+    End Sub
+
+    Private Sub txtUsuario_Click(sender As Object, e As EventArgs) Handles txtUsuario.Click
+        txtUsuario.Text = ""
+        txtUsuario.Focus()
+
+    End Sub
+
+    Private Sub txtContraseña_TextChanged(sender As Object, e As EventArgs) Handles txtContraseña.TextChanged
+
+    End Sub
+
+    Private Sub txtContraseña_Click(sender As Object, e As EventArgs) Handles txtContraseña.Click
+        txtContraseña.Text = ""
+        txtContraseña.Focus()
+    End Sub
+End Class
